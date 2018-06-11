ef0d40da388b554ec94ca6cc177957ccf7fe8b6e
F Freyer <frederic481994@hotmail.de>
2018-06-11 18:20:47 +0200

    ADD basic convergence test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,37 @@ else
 end
 
 # write your own tests here
-@test 1 == 2
+@testset "Checking converging data" begin
+    BA = BinningAnalysis()
+
+    # block of maximally correlated values:
+    N_corr = 16
+
+    # number of blocks
+    N_blocks = 131_072 # 2^17
+    
+    for _ in 1:N_blocks
+        x = rand()
+        for __ in 1:N_corr
+            push!(BA, x)
+        end
+    end
+
+    # BA must diverge until 16 values are binned
+    for lvl in 1:4
+        @test !has_converged(BA, lvl)
+    end
+
+    # Afterwards it must converge (for a while)
+    for lvl in 5:8
+        @test has_converged(BA, lvl)
+    end
+    # Later values may fluctuate due to small samples / small threshold in
+    # has_converged
+
+    # means should be consistent
+    means = all_means(BA)
+    for x in means
+        @test x ≈ means[1]
+    end
+end
