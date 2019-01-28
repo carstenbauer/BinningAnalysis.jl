4131ed237196aeb33652955c49bfcb454d7a1be5
Carsten Bauer <crstnbr@gmail.com>
2019-01-28 17:42:13 +0100

    Base.show cosmetics

--- src/BinningAnalysis.jl
+++ src/BinningAnalysis.jl
@@ -1,8 +1,7 @@
 module BinningAnalysis
 
 import Statistics: mean, var
-import Base.push!
-
+import Base: push!, append!, show, summary
 
 include("binning.jl")
 export LogBinner, push!, append!
@@ -11,5 +10,7 @@ include("statistics.jl")
 export mean, var, varN, tau, std_error
 export all_means, all_vars, all_varNs, all_taus, all_std_errors
 export convergence, has_converged
+
+include("cosmetics.jl")
 
 end # module
---
+++ src/cosmetics.jl
@@ -0,0 +1,22 @@
+function _println_header(io::IO, B::LogBinner{N,T}) where {N, T}
+    "LogBinner{$(N),$(T)}"
+end
+
+function _println_body(io::IO, B::LogBinner{N,T}) where {N, T}
+    n = B.count[1]
+    print("| Count: ", n)
+    if n > 0
+        print("\n| Mean: ", round.(mean(B), digits=5))
+        print("\n| StdError: ", round.(std_error(B), digits=5))
+    end
+end
+
+Base.show(io::IO, B::LogBinner{N,T}) where {N, T} = begin
+    _println_header(io, B)
+    _println_body(io, B)
+    nothing
+end
+Base.show(io::IO, m::MIME"text/plain", B::LogBinner{N,T}) where {N, T} = print(io, B)
+
+Base.summary(io::IO, B::LogBinner{N,T}) where {N, T} = _println_header(io, B)
+Base.summary(B::LogBinner{N,T}) where {N, T} = summary(stdout, B)
