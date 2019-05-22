e6c7b8d47e929d4ea7d7c802d2732b0c03bbe0cb
Frederic Freyer <ffreyer@l45.thp.uni-koeln.de>
2019-05-22 11:40:28 +0200

    add LogBinner-style error propagator

--- src/ErrorPropagation.jl
+++ src/ErrorPropagation.jl
@@ -99,6 +99,7 @@ function var_O1(g::Function, ep::Error_Propagator1)
     result
 end
 
+
 ################################################################################
 ### Version 2: Using Welford's Algorithm for the variance and covariance
 ################################################################################
@@ -145,3 +146,128 @@ ymean(ep::Error_Propagator2) = ep.y_mean
 xvar(ep::Error_Propagator2) = ep.x_var / (ep.N - 1)
 yvar(ep::Error_Propagator2) = ep.y_var / (ep.N - 1)
 xycov(ep::Error_Propagator2) = ep.xy_cov / (ep.N - 1)
+
+
+################################################################################
+### Version 3: Add a Binning Analysis to Version 1
+################################################################################
+
+mutable struct EPCompressor{T}
+    values::Vector{T}
+    switch::Bool
+end
+
+struct Error_Propagator3{T, N} <: Abstract_Error_Propagator
+    compressors::NTuple{N, EPCompressor{T}}
+    # ∑x
+    sums1D::NTuple{N, Vector{T}}
+    #∑xy
+    sums2D::NTuple{N, Matrix{T}}
+
+    count::Vector{Int64}
+end
+
+# Initialization
+function Error_Propagator3(zero::T, N_arguments::Integer, N_levels=32) where T
+    Error_Propagator3{T, N_levels}(
+        tuple([
+            EPCompressor{T}([copy(zero) for _ in 1:N_arguments], false)
+            for __ in 1:N_levels
+        ]...),
+        tuple([
+            [copy(zero) for _ in 1:N_arguments]
+            for __ in 1:N_levels
+        ]...),
+        tuple([
+            [copy(zero) for _ in 1:N_arguments, __ in 1:N_arguments]
+            for __ in 1:N_levels
+        ]...),
+        zeros(Int64, N_levels)
+    )
+end
+
+function Base.push!(ep::Error_Propagator3{T, N}, args::T...) where {T, N}
+    _push!(ep, 1, args)
+end
+
+function _push!(ep::Error_Propagator3{T, N}, lvl::Int64, args) where {T, N}
+    # TODO Remove "..."?
+    C = ep.compressors[lvl]
+
+    # any value propagating through this function is new to lvl. Therefore we
+    # add it to the sums. Note that values pushed to the output arrays are not
+    # added here until the array drops to the next level. (New compressors are
+    # added)
+    ep.count[lvl] += 1
+    # don't do any of this, it's bad
+    # @views ep.sums1D[lvl] .+= args
+    # @views ep.sums2D[lvl] .+= args * args'
+
+    # do this
+    @simd for i in eachindex(ep.sums1D[lvl])
+        ep.sums1D[lvl][i] += args[i]
+        @inbounds for j in eachindex(ep.sums1D[lvl])
+            ep.sums2D[lvl][i, j] += args[i] * args[j]
+        end
+    end
+
+    if !C.switch
+        # Compressor has space -> save value
+        C.values .= args
+        C.switch = true
+        return nothing
+    else
+        # Do averaging
+        if lvl == N
+            # No more propagation possible -> throw error
+            throw(OverflowError("The Error Propagator has exceeded its maximum capacity."))
+        else
+            # propagate to next lvl
+            C.switch = false
+            _push!(ep, lvl+1, 0.5 * (C.values .+ args))
+            return nothing
+        end
+    end
+    return nothing
+
+
+end
+
+
+means(ep::Error_Propagator3, lvl) = ep.sums1D[lvl] ./ ep.count[lvl]
+function covmat(ep::Error_Propagator3, lvl)
+    invN = 1.0 / ep.count[lvl]
+    invN1 = 1.0 / (ep.count[lvl] - 1)
+    [
+        (ep.sums2D[lvl][i, j] - ep.sums1D[lvl][i] * ep.sums1D[lvl][j] * invN) * invN1
+        for i in eachindex(ep.sums1D[lvl]), j in eachindex(ep.sums1D[lvl])
+    ]
+end
+
+function var_O1(g::Function, ep::Error_Propagator3, lvl)
+    # NOTE: Not type stable :(
+    # derivatives = ForwardDiff.gradient(v -> g(v...), means(ep))
+    # NOTE: Also not type stable
+    # NOTE: crossing a function barrier doesn"t hel either
+    ms = means(ep, lvl)
+    derivatives = [
+        ForwardDiff.derivative(
+            x -> g(ms[1:i-1]..., x, ms[i+1:end]...),
+            ms[i]
+        ) for i in eachindex(ms)
+    ]
+
+
+    result = 0.0
+    invN = 1.0 / ep.count[lvl]
+    invNN1 = 1.0 / (ep.count[lvl] * (ep.count[lvl] - 1))
+    for i in eachindex(ep.sums1D[lvl])
+        for j in eachindex(ep.sums1D[lvl])
+            result += derivatives[i] * derivatives[j] * (
+                ep.sums2D[lvl][i, j] - ep.sums1D[lvl][i] * ep.sums1D[lvl][j] * invN
+            ) * invNN1
+        end
+    end
+
+    result
+end
