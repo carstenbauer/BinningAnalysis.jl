36ef356d4c9631c3e822ca878cf56e3c9f4bc4cc
ffreyer <frederic481994@hotmail.de>
2022-09-19 11:43:29 +0200

    add tests for Arrays and missed functions

--- test/PreBinner.jl
+++ test/PreBinner.jl
@@ -29,7 +29,11 @@ for wrapped in (LogBinner, FullBinner)
         
         @test B.binner isa wrapped
         @test length(B) == 0
-        wrapped == LogBinner && (@test count(B) == 0)
+        if wrapped == LogBinner
+            @test count(B) == 0
+            @test BinningAnalysis.nlevels(B) == 1 + BinningAnalysis.nlevels(B.binner)
+            @test BinningAnalysis.capacity(B) == length(B)
+        end
         @test ndims(B) == 0
         @test isempty(B)
         @test eltype(B) == Float64
@@ -54,6 +58,31 @@ for wrapped in (LogBinner, FullBinner)
 
         @test mean(B) ≈ mean(xs) # Float errors
         @test std_error(B) == std_error(B.binner)
+        @test var(B) == var(B.binner)
         @test BinningAnalysis._reliable_level(B) == 1 + BinningAnalysis._reliable_level(B.binner)
+
+
+        empty!(B)
+
+        @test B == B2
+        @test B.n == 0
+        @test length(B) == 0
+        @test length(B.binner) == 0
     end
+end
+
+@testset "Array elements" begin
+    B = PreBinner(LogBinner(zeros(3, 3)), 7)
+    StableRNGs.seed!(rng, 123)
+    xs = [rand(rng, 3, 3) for _ in 1:100]
+    append!(B, xs)
+
+    @test B.n == 100 % 7
+    @test length(B) == 100
+    @test length(B.binner) == div(100, 7)
+
+    @test mean(B) ≈ mean(xs) # Float errors
+    @test std_error(B) == std_error(B.binner)
+    @test var(B) == var(B.binner)
+    @test BinningAnalysis._reliable_level(B) == 1 + BinningAnalysis._reliable_level(B.binner)
 end
